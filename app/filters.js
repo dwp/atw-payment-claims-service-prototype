2a72f998cda836841b03d2713fc2ed4c69ef6208
“dan040796“ <dan.lander@engineering.digital.dwp.gov.uk>
2023-09-05 16:22:05 +0100

    TiW changes from UR

--- app/filters.js
+++ app/filters.js
@@ -7,6 +7,19 @@ module.exports = function (env) {
    */
   var filters = {}
 
+  var special = ['zeroth','first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
+  var deca = ['twent', 'thirt', 'fort', 'fift', 'sixt', 'sevent', 'eight', 'ninet'];
+
+  filters.stringifyNumber = function (n) {
+    if (n < 20) return special[n];
+    if (n%10 === 0) return deca[Math.floor(n/10)-2] + 'ieth';
+    return deca[Math.floor(n/10)-2] + 'y-' + special[n%10];
+  }
+
+  filters.capitalizeFirstLetter = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   filters.numberMonth = function (i) {
 
     if (i == '1' || i == '01') {
